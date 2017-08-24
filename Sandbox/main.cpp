c5e5dc9f5772d985397bd5a2184c384a6c84224f
Kevin Russell <kev_kix@hotmail.com>
2017-08-24 07:53:02 +0100

    Tuple struct that should work but does not

--- Sandbox/main.cpp
+++ Sandbox/main.cpp
@@ -3,6 +3,7 @@
 #include <tuple>
 #include <string>
 
+// INITIALIZATION
 template <class... Ts> struct tuple {}; // Base case
 
 template <class T, class... Ts>
@@ -12,18 +13,39 @@ struct tuple<T, Ts...> : tuple<Ts...> { // tuple<T, Ts...> is inherited from tup
 	T tail;
 };
 
+// TYPE FUNCTIONS
 template <class T, class... Ts>
 struct elem_type_holder<0, tuple<T, Ts...>> {
 	typedef T type;
 };
 
-template <size_t, class T, class... Ts>
+template <size_t k, class T, class... Ts>
 struct elem_type_holder<k, tuple<T, Ts...>> {
 	typedef typename elem_type_holder<k - 1, tuple<Ts...>>::type type;
 };
 
+
+// GET
+template <size_t k, class... Ts>
+typename std::enable_if<
+	k == 0, typename elem_type_holder<0, tuple<Ts...>>::type&>::type
+	get(tuple<Ts...>& t) {
+	return t.tail;
+}
+
+template <size_t k, class T, class... Ts>
+typename std::enable_if<
+	k != 0, typename elem_type_holder<k, tuple<T, Ts...>>::type&>::type
+	get(tuple<T, Ts...>& t) {
+	tuple<Ts...>& base = t;
+	return get<k - 1>(base);
+}
+
+
+
 int main() {
 	tuple<double, uint64_t, const char*> t1(12.2, 42, "big");
+	auto t2 = get<0>(t1);
 	int t = 1;
 }
 
