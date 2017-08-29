d27c1a44212e6a184565e1d43838076c7c7f3e88
Kevin Russell <kev_kix@hotmail.com>
2017-08-29 08:23:54 +0100

    array_type example from The C++ Programming Language

--- Sandbox/main.cpp
+++ Sandbox/main.cpp
@@ -2,72 +2,20 @@
 #include <iostream>
 #include <tuple>
 #include <string>
+#include <array>
 
-// INITIALIZATION
-template <class... Ts> struct tuple {}; // Base case
-
-template <class T, class... Ts>
-struct tuple<T, Ts...> : tuple<Ts...> { // tuple<T, Ts...> is inherited from tuple<Ts...>
-	tuple(T t, Ts... ts) : tuple<Ts...>(ts...), tail(t) {} // Constructor with initializer list
-
-	T tail;
+template <typename T, int N>
+struct Array_type {
+	using type = T;
+	static const int dim = N;
 };
-
-// TYPE FUNCTIONS
-template <class T, class... Ts>
-struct elem_type_holder<0, tuple<T, Ts...>> {
-	typedef T type;
-};
-
-template <size_t k, class T, class... Ts>
-struct elem_type_holder<k, tuple<T, Ts...>> {
-	typedef typename elem_type_holder<k - 1, tuple<Ts...>>::type type;
-};
-
-
-// GET
-template <size_t k, class... Ts>
-typename std::enable_if<
-	k == 0, typename elem_type_holder<0, tuple<Ts...>>::type&>::type
-	get(tuple<Ts...>& t) {
-	return t.tail;
-}
-
-template <size_t k, class T, class... Ts>
-typename std::enable_if<
-	k != 0, typename elem_type_holder<k, tuple<T, Ts...>>::type&>::type
-	get(tuple<T, Ts...>& t) {
-	tuple<Ts...>& base = t;
-	return get<k - 1>(base);
-}
-
-
-template<class T>
-struct listType {
-	bool isEmpty() const;
-	bool isFull() const;
-	bool search(const T&) const;
-	void insert(const T&);
-	void remove(const T&);
-	void destroyList();
-	void printList();
-	listType();
-private:
-	T list[100];
-	int length;
-};
-
-template <class T>
-void listType<T>::insert(const T& newElement) {
-	//function stuff
-}
-
 
 
 
 int main() {
-	tuple<double, uint64_t, const char*> t1(12.2, 42, "big");
-	auto t2 = get<0>(t1);
-	int t = 1;
+	using Array = Array_type<int, 3>;
+	Array::type x;						// x is an int
+	constexpr int s = Array::dim;		// s is 3
+	int a = 1;
 }
 
