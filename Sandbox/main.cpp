4f991757e317eead2edc266d7a7368e920d6e716
Kevin Russell - Surface <kev_kix@hotmail.com>
2017-09-01 09:03:45 +0100

    First part of aherrmann tutorial on varidac tuples with templates

--- Sandbox/main.cpp
+++ Sandbox/main.cpp
@@ -5,27 +5,30 @@
 #include <array>
 
 //https://www.murrayc.com/permalink/2015/12/05/modern-c-variadic-template-parameters-and-tuples/
+//http://aherrmann.github.io/programming/2016/02/28/unpacking-tuples-in-cpp14/
 
-template <class... T_values>
-class Base {
-	virtual void something(T_values... values) = 0;
-};
+//First define a type that can hold a parameter pack of indeces
+template<size_t... Is>
+struct index_sequence;
 
-class Derived1 : public Base<int, short, double> {
-public:
-	void something(int a, short b, double c) override;
-};
+//Next use parameter pack expansion to index into a generic tuple
+template <class Tuple, size_t... Is>
+void function(Tuple t, index_sequence<Is...>) {
+	some_func(std::get<Is>(t)...);
+}
 
-class Derived2 : public Base<std::string, char> {
-	void something(std::string a, char b) override;
-};
-
+template <typename T, typename... Args>
+int some_func(T t, Args... args) {
+	return sizeof(t);
+}
 
 
 
 
 int main() {
-	Base.something(int a);
+	std::tuple<int, char, float>t1 = { 2, 'a', 5.6f };
+	function(t1, index_sequence<0,1,2>);
+	int a = 8;
 
 }
 
