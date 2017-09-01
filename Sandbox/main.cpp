caa9c3f683ee1e7babd375609b9b5797d9dbdddb
Kevin Russell <kev_kix@hotmail.com>
2017-09-01 07:37:34 +0100

    Select struct (shorter version using variadic templates)

--- Sandbox/main.cpp
+++ Sandbox/main.cpp
@@ -4,47 +4,31 @@
 //#include <string>
 //#include <array>
 
-class Nil {};
+template<unsigned N, typename... Cases> // General case, never instantiated
+struct select {};
 
-template<int I, typename T1 = Nil, typename T2 = Nil, typename T3 = Nil, typename T4 = Nil>
-struct select;
+template<unsigned N, typename T, typename... Cases>
+struct select<N, T, Cases...> : select<N - 1, Cases...> {
+};
 
-template<int I, typename T1 = Nil, typename T2 = Nil, typename T3 = Nil, typename T4 = Nil>
-using Select = typename select<I, T1, T2, T3, T4>;
+template <typename T, typename... Cases>
+struct select<0, T, Cases...> {
+	using type = T;
+};
 
-// Specializations for 0 - 3:
+template <unsigned N, typename... Cases>
+using Select = typename select<N, Cases...>::type;
 
-template<typename T1, typename T2, typename T3, typename T4>
-struct select<0, T1, T2, T3, T4> { using type = T1; };
-
-template<typename T1, typename T2, typename T3, typename T4>
-struct select<1, T1, T2, T3, T4> { using type = T2; };
-
-template<typename T1, typename T2, typename T3, typename T4>
-struct select<2, T1, T2, T3, T4> { using type = T3; };
-
-template<typename T1, typename T2, typename T3, typename T4>
-struct select<3, T1, T2, T3, T4> { using type = T4; };
-
-template<int N, typename T1, typename T2, typename T3, typename T4>
-Select<N, T1, T2, T3, T4>& get(Tuple<T1, T2, T3, T4>& t);
-
-
-
-//template<int N>
-//struct Integer {
-//	using Error = void;
-//	using type = Select<N, Error, signed char, short>;
-//};
-
-
+template<int N>
+struct Integer {
+	using Error = void;
+	using type = Select < N, Error, signed char, short, Error, int, Error, Error, Error, long>;
+};
 
 int main() {
 
-	std::tuple<int, float, char> t(3, 4.5f, 'a');
-	auto x = get<2>(t);
-	int a = 3;
-	//typename Integer<4>::type i4 = 8;
-	//typename Integer<1>::type i1 = 9;
+	typename Integer<4>::type i4 = 8;
+	typename Integer<1>::type i1 = 9;
+	int a = 0;
 }
 
