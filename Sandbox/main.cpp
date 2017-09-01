695a26dd5d7987153e9db9a021bdc604b8d07c72
Kevin Russell - Surface <kev_kix@hotmail.com>
2017-09-01 09:12:30 +0100

    Starting on stack overflow example on how to create an index sequence

--- Sandbox/main.cpp
+++ Sandbox/main.cpp
@@ -7,6 +7,24 @@
 //https://www.murrayc.com/permalink/2015/12/05/modern-c-variadic-template-parameters-and-tuples/
 //http://aherrmann.github.io/programming/2016/02/28/unpacking-tuples-in-cpp14/
 
+//log N way of constructing index_sequence from 
+//https://stackoverflow.com/questions/17424477/implementation-c14-make-integer-sequence/17426611#17426611
+
+//using aliases for cleaner syntax
+template<class T> using Invoke = typename T::type;
+
+template<unsigned...> struct index_sequence { 
+	using type = index_sequence; 
+};
+
+template<unsigned... I1, unsigned... I2>
+struct concat<index_sequence<I1...>, index_sequence<I2...>>
+	: seq<I1..., (sizeof...(I1)+I2)...> {};
+
+template<class S1, class S2>
+using Concat = Invoke<concat<S1, S2>>;
+
+
 //First define a type that can hold a parameter pack of indeces
 template<size_t... Is>
 struct index_sequence;
@@ -27,7 +45,7 @@ int some_func(T t, Args... args) {
 
 int main() {
 	std::tuple<int, char, float>t1 = { 2, 'a', 5.6f };
-	function(t1, index_sequence<0,1,2>);
+	//function(t1, index_sequence<0,1,2>);
 	int a = 8;
 
 }
