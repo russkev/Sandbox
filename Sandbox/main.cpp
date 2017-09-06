d9def09ccd27806be770f4387f7cbb40356dd3a5
Kevin Russell <kev_kix@hotmail.com>
2017-09-06 07:22:01 +0100

    Obj_holder with On_heap and Scoped

--- Sandbox/main.cpp
+++ Sandbox/main.cpp
@@ -1,34 +1,63 @@
 //Move semantics
 //#include <iostream>
 #include <tuple>
+#include <vector>
 //#include <string>
-//#include <array>
+#include <array>
 
-template<unsigned N, typename... Cases> // General case, never instantiated
-struct select {};
+template<typename T>
+struct On_heap {
+	On_heap() :p(new T) {}		//allocate
+	~On_heap() { delete p; }	//deallocate
 
-template<unsigned N, typename T, typename... Cases>
-struct select<N, T, Cases...> : select<N - 1, Cases...> {
+	T& operator*() { return *p; }
+	T* operator->() { return p; }
+
+	On_heap(const On_heap&)				= delete;	//prevent copying
+	On_heap& operator=(const On_heap&)	= delete;	//prevent assigning
+private:
+	T* p;
 };
 
-template <typename T, typename... Cases>
-struct select<0, T, Cases...> {
-	using type = T;
+template<typename T>
+struct Scoped {
+	Scoped() {}
+
+	T& operator*() { return x; }
+	T* operator->() { return &x; }
+
+	Scoped(const Scoped&)				= delete;
+	Scoped& operator = (const Scoped&)	= delete;
+private:
+	T x;
 };
 
-template <unsigned N, typename... Cases>
-using Select = typename select<N, Cases...>::type;
 
-template<int N>
-struct Integer {
-	using Error = void;
-	using type = Select < N, Error, signed char, short, Error, int, Error, Error, Error, long>;
+
+constexpr int on_stack_max = sizeof(std::string);	//Max size of object we want on stack
+
+template<typename T>
+struct Obj_holder {
+	using type = typename std::conditional < (sizeof(T) <= on_stack_max), Scoped<T>, On_heap<T>> ::type;
 };
+
+
+
+template <typename T>
+using Holder = typename Obj_holder<T>::type;
+
+void f2() {
+	Holder<double> v1;					// The double goes on the stack
+	Holder<std::array<double, 200>> v2;	// The array goes on the free store
+	*v1 = 7.7;							// Scoped provides pointer like access
+	(*v2)[77] = 9.9;					// On_heap provides pointer like access
+}
 
 int main() {
 
-	typename Integer<4>::type i4 = 8;
-	typename Integer<1>::type i1 = 9;
+
+	f2();
+
 	int a = 0;
 }
 
