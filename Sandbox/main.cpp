03e80cebe9cd36cb9ff9af9d6a54050a1eecc560
Kevin Russell - Surface <kev_kix@hotmail.com>
2017-09-12 09:00:41 +0100

    find_biggest type

--- Sandbox/main.cpp
+++ Sandbox/main.cpp
@@ -14,33 +14,43 @@
 template<size_t... Entries>
 struct StaticArray
 {
-	//enum {N = 4};
-	//enum { N = sizeof...(Entries) };
-	//size_t m_size = sizeof... (Entries);
 	std::vector<size_t> m_vec = { Entries... };
 	size_t m_array [sizeof...(Entries)] = { Entries... };
 };
 
+template<typename... Args>
+struct find_biggest {};
 
+//If there is only one thing, it is the biggest
+template<typename First>
+struct find_biggest<First>
+{
+	typedef First type;
+};
 
+//The biggest of everything in Args and First
+template <typename First, typename... Args>
+struct find_biggest<First, Args...>
+{
+	typedef typename find_biggest<Args...>::type next;
+	typedef typename std::conditional<
+		sizeof(First) >= sizeof(next),
+		First,
+		next
+	>::type type;
+};
 
-template <typename T, typename ...Args>
-T t1(T t, Args... args) {
-	return t1(args...);
-}
-
-template <typename T>
-T t1(T t) {
-	return t;
-}
 
 
 int main() {
 	int a = 1;
 	float b = 2.3f;
 	double c = 4.65;
-	auto test = t1(a, b, c);
 	StaticArray<3, 4, 5, 8, 9, 10> sa1;
+
+	typedef find_biggest<char, float, double>::type fb_type;
+
+	fb_type fb2 = 23;
 
 	std::tuple<int, char, float>t1 ( 2, 'a', 5.6f );
 	//function(t1, index_sequence<0,1,2>);
