69f58b2688f711c9c4863835d601af3151f548d3
Kevin Russell - Surface <kev_kix@hotmail.com>
2017-09-18 09:31:11 +0100

    Re-creating the previous struct

--- Sandbox/main.cpp
+++ Sandbox/main.cpp
@@ -9,48 +9,18 @@
 //http://aherrmann.github.io/programming/2016/02/28/unpacking-tuples-in-cpp14/
 //https://thenewcpp.wordpress.com/2011/11/23/variadic-templates-part-1-2/
 
-
-
-template<size_t... Entries>
-struct StaticArray
-{
-	std::vector<size_t> m_vec = { Entries... };
-	size_t m_array [sizeof...(Entries)] = { Entries... };
-};
-
 template<typename... Args>
-struct find_biggest;
+struct Biggest;
 
-//If there is only one thing, it is the biggest
 template<typename First>
-struct find_biggest<First>
-{
-	typedef First type;
+struct Biggest<First> {
+	using type = First;
 };
 
-//The biggest of everything in Args and First
-template <typename First, typename... Args>
-struct find_biggest<First, Args...>
-{
-	using next = typename find_biggest<Args...>::type;
-	using type = typename std::conditional<
-		sizeof(First) >= sizeof(next),
-		First,
-		next
-	>::type;
-};
-
-template <typename... Args>
-struct find_last;
-
-template <typename First>
-struct find_last<First> {
-	typedef First type;
-};
-
-template <typename First, typename... Args>
-struct find_last<First, Args...> {
-	typedef typename find_last<Args...>::type type;
+template<typename First, typename... Args>
+struct Biggest<First, Args...> {
+	using next = typename Biggest<Args...>::type;
+	using type = typename std::conditional<sizeof(First) < sizeof(next), next, First>::type;
 };
 
 
@@ -59,17 +29,11 @@ int main() {
 	int a = 1;
 	float b = 2.3f;
 	double c = 4.65;
-	StaticArray<3, 4, 5, 8, 9, 10> sa1;
 
-	typedef find_biggest<char, float, double>::type fb_type;
-	using ff_type = find_last<int, double, char>::type;
+	using BA = Biggest<int, char, double>::type;
+	BA BB = 45;
 
-	fb_type fb2 = 23;
-	ff_type ff2 = 24;
-
-	std::tuple<int, char, float>t1 ( 2, 'a', 5.6f );
-	//function(t1, index_sequence<0,1,2>);
-	int z = 8;
+	int x = 1;
 
 }
 
