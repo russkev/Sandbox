cd09b0164f57828f16bf2ab7e73396d5f256c7a5
Kevin Russell - Surface <kev_kix@hotmail.com>
2018-02-12 15:08:35 +0000

    Still working on sorting

--- Sandbox/main.cpp
+++ Sandbox/main.cpp
@@ -9,30 +9,40 @@
 int main() {
 
 
-	std::vector<int> numList = { 54, 5, 3, 9, 0, -2, 10, 12, 45, 23, 12, 12, 9, 345, 23 };
-	std::vector<int> sortedList = { numList.at(0) };
+	std::vector<int> numList		= { 54, 5, 3, 9, 0, -2, 10, 12, 45, 23, 12, 12, -23, 9, 345, 23 };
+	std::vector<int> sortedList		= { numList.at(0) };
 
 	for (auto i = 1; i < numList.size(); ++i)
 	{
 		std::cout << i;
-		auto start	= sortedList.begin();
-		auto end	= sortedList.end();
-		auto middle = start + (sortedList.size() / 2);
+		auto it_start			= sortedList.begin();
+		auto it_end				= sortedList.end();
+		auto it_middle			= it_start + (sortedList.size() / 2);
+		auto it_insert_point	= it_start;
 
-		while (start != end)
+		//while (it_start != it_end || it_start + 1 != it_end)
+		//while (it_start != it_middle)
+		while (it_start + 1 != it_end)
 		{
-			if (numList[i] > *middle)
+			if (numList[i] == *it_middle)
 			{
-				start = middle;
-				middle = middle + ((end - middle) / 2);
+				it_insert_point = it_middle;
+				break;
+			}
+			if (numList[i] > *it_middle)
+			{
+				it_start		= it_middle;
+				it_middle		= it_middle + ((it_end - it_middle) / 2);
+				it_insert_point = it_middle + 1;
 			}
 			else
 			{
-				end = middle;
-				middle = middle - ((middle - start) / 2);
+				it_end			= it_middle;
+				it_middle		= it_middle - ((it_middle - it_start) / 2);
+				it_insert_point = it_middle;
 			}
 		}
-		sortedList.insert(middle, numList[i]);
+		sortedList.insert(it_insert_point, numList[i]);
 	}
 	auto x = 0;
 }
